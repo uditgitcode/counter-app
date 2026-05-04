680509a8d865a66f2dea374e2b0a018ebd0eb5c7
uditgitcode <uditsrikumar@gmail.com>
2026-05-04 10:00:03 +0530

    Add files via upload

---
+++ web dev practical/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./Components/Counter";
+
+function App() {
+  return (
+    <div>  
+        <h1> Welcome to My App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
